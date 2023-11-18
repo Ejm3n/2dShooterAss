5033b9ea935378152e11d79061277a146bfaf2e2
Ejm3n <yashkaydakov@gmail.com>
2023-11-18 16:50:00 -0500

    object pool for bullets and borders only for playr

--- Assets/Scripts/Shotgun.cs
+++ Assets/Scripts/Shotgun.cs
@@ -7,14 +7,20 @@ public class Shotgun : MonoBehaviour
     [SerializeField]
     private float force;
 
-    [SerializeField] private GameObject bullet;
+    [SerializeField] private Bullet bullet;
     [SerializeField] private GameObject shootPoint;
+    private ObjectPool<Bullet> pool;
 
-
+    private void Awake()
+    {
+        pool = new ObjectPool<Bullet>(bullet, 10, shootPoint.transform);
+        pool.AutoExpand = true;
+    }
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
@@ -29,6 +35,10 @@ public class Shotgun : MonoBehaviour
 
     void shoot()
     {
-        Instantiate(bullet, shootPoint.transform);
+        Bullet newBullet = pool.GetFreeElement();
+        newBullet.gameObject.SetActive(true);
+        newBullet.transform.position = shootPoint.transform.position;
+        newBullet.transform.rotation = transform.rotation;
+        //Instantiate(bullet, shootPoint.transform);
     }
 }
