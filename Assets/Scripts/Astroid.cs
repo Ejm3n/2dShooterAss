08a5f929d1444df503e3e6492462f33059a53c1e
Ejm3n <yashkaydakov@gmail.com>
2023-11-18 17:50:47 -0500

    update

--- Assets/Scripts/Astroid.cs
+++ Assets/Scripts/Astroid.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class Astroid : Bullet
 {
+    [SerializeField] private GameObject explosionPrefab;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
             GameManager.Instance.GameOver();
             gameObject.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.CompareTag("Bullet"))
+        {
+            GameObject exp = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(exp,3f);
         }
     }
 }
